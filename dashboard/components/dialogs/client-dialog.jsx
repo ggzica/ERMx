9895997954ba594a349cb4027033c09236ea69df
unknown <georgio.ghazzi.ca@gmail.com>
2025-06-05 13:43:41 -0400

    Switching to laptop

--- dashboard/components/dialogs/client-dialog.jsx
+++ dashboard/components/dialogs/client-dialog.jsx
@@ -11,7 +11,6 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { DialogCloseButton } from '@/components/dialogs/client-dialog'
 export function DialogDemo() {
   return (
     <Dialog>
